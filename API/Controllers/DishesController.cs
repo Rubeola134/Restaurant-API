77d3bf45042f9488612cd3a8e44eeec4e1e193c8
Ruben Garcia <rubendanielgarciat@gmail.com>
2026-02-11 17:33:54 -0500

    Added Dish Post to DishController

--- API/Controllers/DishesController.cs
+++ API/Controllers/DishesController.cs
@@ -1,29 +1,20 @@
 using API.Dtos;
 using API.Helpers.Commands.CreateDishCommand;
 using MediatR;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
-using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
-
 
 namespace API.Controllers
 {
-    [Route("api/restaurants/{restaurantId}/[controller]")]
+    [Route("api/restaurants/{restaurantId}/dishes")]
     [ApiController]
     public class DishesController(IMediator mediator) : ControllerBase
     {
-        [HttpPost ("add")]
+        [HttpPost("add")]
         public async Task<ActionResult> AddDish([FromRoute]int restaurantId, [FromBody] CreateDishCommand command)
         {
             command.RestaurantId = restaurantId;
             await mediator.Send(command);
             return Ok();
         }
-
-        // [HttpGet("dishes")]
-        // public async Task<ActionResult<List<DishDto>>> GetDishes()
-        // {
-
-        // }
     }
 }
